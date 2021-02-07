59c9416a6d442db1e4f7581c9f225ad52b3bd25a
Gxx-5 <1>
2021-02-07 10:37:30 +0800

    purify

--- Examples/ROS/ORB_SLAM2/src/ros_mono_pub.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono_pub.cc
@@ -24,10 +24,13 @@
 #include<fstream>
 #include<chrono>
 #include <time.h>
+#include <iomanip>
 
 #include<ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
+#include "sensor_msgs/PointCloud.h"
 #include "sensor_msgs/PointCloud2.h"
+#include <sensor_msgs/point_cloud_conversion.h>
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/PoseArray.h"
 #include "visualization_msgs/MarkerArray.h"
@@ -44,16 +47,13 @@
 #include <opencv2/highgui/highgui_c.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <Converter.h>
-#include "Explore/include/costcube.h"
+// #include "CalCost.h"
 
 //! parameters
 bool read_from_topic = true, read_from_camera = false;
 // double nearbyradius = 1.0;
 //Publish
 ros::Publisher pub_cloud;
-ros::Publisher pub_map_cloud;
-ros::Publisher pub_cur_view_cloud;
-ros::Publisher vis_pub,vis_text_pub;
 bool save_to_results = false;
 std::string image_topic = "/camera/image_raw";
 int all_pts_pub_gap = 0;
@@ -64,22 +64,11 @@ cv::VideoCapture cap_obj;
 
 bool pub_all_pts = false;
 int pub_count = 0;
-double filter_radius = 0.5;
-double focal_len = 1.5;
-double field_size = 0.5;
-double costcube_resolution = 0.1;
-cv::Mat costcube_map;
-CostCube COSTCUBE(focal_len,field_size,costcube_resolution);
 
-void LoadImages(const string &strSequence, vector<string> &vstrImageFilenames,
-	vector<double> &vTimestamps);
+void LoadImages(const string &strSequence, vector<string> &vstrImageFilenames,vector<double> &vTimestamps);
 inline bool isInteger(const std::string & s);
 void publish(ORB_SLAM2::System &SLAM, ros::Publisher &pub_pts_and_pose,
 			 ros::Publisher &pub_all_kf_and_pts, ros::Publisher &pub_cur_camera_pose, int frame_id);
-void FilterNearbyPoint(ORB_SLAM2::Map* map,std::vector<float> CamPos);
-void PublishMapPointstoCloud(std::vector<ORB_SLAM2::MapPoint*> points,ros::Publisher publisher,
-								vector<geometry_msgs::Point>* points_pos = NULL);
-void VisualizeCostCube(cv::Mat cost_map,geometry_msgs::Pose camera_pose);
 
 class ImageGrabber{
 public:
@@ -111,16 +100,11 @@ int main(int argc, char **argv){
 	// Create SLAM system. It initializes all system threads and gets ready to process frames.
 	ORB_SLAM2::System SLAM(argv[1], argv[2], ORB_SLAM2::System::MONOCULAR, true);
 	ros::NodeHandle nodeHandler;
-	pub_cloud = nodeHandler.advertise<sensor_msgs::PointCloud2>("cloud_in", 1000);
-	pub_map_cloud = nodeHandler.advertise<sensor_msgs::PointCloud2>("map_cloud", 1000);
-	pub_cur_view_cloud = nodeHandler.advertise<sensor_msgs::PointCloud2>("cur_map_cloud", 1000);
-	vis_pub = nodeHandler.advertise<visualization_msgs::MarkerArray>("CostCube",10);
-	vis_text_pub = nodeHandler.advertise<visualization_msgs::MarkerArray>("CostCubeText",10);
+	pub_cloud = nodeHandler.advertise<sensor_msgs::PointCloud>("/cur_kp_cloud", 1000);
 	ros::Publisher pub_pts_and_pose = nodeHandler.advertise<geometry_msgs::PoseArray>("pts_and_pose", 1000);
 	ros::Publisher pub_all_kf_and_pts = nodeHandler.advertise<geometry_msgs::PoseArray>("all_kf_and_pts", 1000);
 	// ros::Publisher pub_cur_camera_pose = nodeHandler.advertise<geometry_msgs::Pose>("/cur_camera_pose", 1000);
-	 ros::Publisher pub_cur_camera_pose = nodeHandler.advertise<geometry_msgs::PoseStamped>("/cur_camera_pose", 1000);
-	//Param 
+	ros::Publisher pub_cur_camera_pose = nodeHandler.advertise<geometry_msgs::PoseStamped>("/cur_camera_pose", 1000);
 
 	if (read_from_topic) {
 		ImageGrabber igb(SLAM, pub_pts_and_pose, pub_all_kf_and_pts, pub_cur_camera_pose);
@@ -295,11 +279,6 @@ void publish(ORB_SLAM2::System &SLAM, ros::Publisher &pub_pts_and_pose,
 		// std::vector<ORB_SLAM2::MapPoint*> all_map_points = SLAM.getMap()->GetAllMapPoints();
 		std::vector<ORB_SLAM2::MapPoint*> map_points = SLAM.GetTrackedMapPoints();
 		int n_map_pts = map_points.size();
-		// int all_map_pts = all_map_points.size();
-
-		// printf("\ntracked_map_pts: %d\n", n_map_pts);
-		// printf("all_map_pts: %d\n", all_map_pts);
-
 		pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
 		geometry_msgs::PoseArray pt_array;
@@ -349,7 +328,9 @@ void publish(ORB_SLAM2::System &SLAM, ros::Publisher &pub_pts_and_pose,
 
 		// printf("valid map pts: %lu\n", pt_array.poses.size()-1);				
 		// printf("ros_cloud size: %d x %d\n", ros_cloud.height, ros_cloud.width);
-		pub_cloud.publish(ros_cloud);
+		sensor_msgs::PointCloud ros_cloud1;
+		sensor_msgs::convertPointCloud2ToPointCloud(ros_cloud,ros_cloud1);
+		pub_cloud.publish(ros_cloud1);
 
 		pt_array.header.frame_id = "map";
 		pt_array.header.seq = frame_id + 1;
@@ -375,186 +356,17 @@ void publish(ORB_SLAM2::System &SLAM, ros::Publisher &pub_pts_and_pose,
 		camera_pose.orientation.z = q[2];
 		camera_pose.orientation.w = q[3];
 		
-		geometry_msgs::PoseStamped camera_poseStamped;
-		camera_poseStamped.pose = camera_pose;
-		camera_poseStamped.header.frame_id = "map";
-		camera_poseStamped.header.stamp = ros::Time::now();
-		pub_cur_camera_pose.publish(camera_poseStamped);
+		geometry_msgs::PoseStamped camera_posestamped;
+		camera_posestamped.pose = camera_pose;
+		camera_posestamped.header.frame_id = "map";
+		camera_posestamped.header.stamp = ros::Time::now();
+		pub_cur_camera_pose.publish(camera_posestamped);
 		//Modify Map points nearby camear by the way
 		// FilterNearbyPoint(SLAM.getMap(),std::vector<float>{twc.at<float>(0),twc.at<float>(1),twc.at<float>(2)});
 	}
-	//Publish map points
-	vector<geometry_msgs::Point> map_points_pos;
-	// std::vector<ORB_SLAM2::MapPoint*> all_map_points = SLAM.getMap()->GetAllMapPoints();	
-	// PublishMapPointstoCloud(all_map_points,pub_map_cloud,&map_points_pos);
-	std::vector<ORB_SLAM2::MapPoint*> cur_view_points = SLAM.GetTrackedMapPoints();
-	PublishMapPointstoCloud(cur_view_points,pub_cur_view_cloud,&map_points_pos);
-	//Publish nearby map points
-	// std::vector<ORB_SLAM2::MapPoint*> nearby_points = SLAM.getMap()->GetNearbyMapPoints();
-	// vector<geometry_msgs::Point> nearby_points_pos;
-	// PublishMapPointstoCloud(nearby_points,pub_cur_map_cloud,&nearby_points_pos);
-	costcube_map = COSTCUBE.calCostCubeByDistance(map_points_pos,camera_pose);
-	VisualizeCostCube(costcube_map,camera_pose);
+	// mainProcess(SLAM,pub_cur_view_cloud,camera_pose);
 }
 
-void FilterNearbyPoint(ORB_SLAM2::Map* map,std::vector<float> CamPos){
-	std::vector<ORB_SLAM2::MapPoint*> nearby_points = map->GetNearbyMapPoints();
-	int num_pts = nearby_points.size();
-	int num_filtered = 0;
-	for (int pt_id = 1; pt_id <= num_pts; ++pt_id){
-		if (!nearby_points[pt_id - 1] || nearby_points[pt_id - 1]->isBad()) {
-			// printf("Point %d is bad\n", pt_id);
-			continue;
-		}
-		cv::Mat wp = nearby_points[pt_id - 1]->GetWorldPos();
-
-		if (wp.empty()) {
-			// printf("World position for point %d is empty\n", pt_id);
-			continue;
-		}
-		double dst = sqrt(pow((wp.at<float>(0)-CamPos[0]),2)+pow((wp.at<float>(1)-CamPos[1]),2)+pow((wp.at<float>(2)-CamPos[2]),2));
-		// cout << "Distance from current point  is " << dst << endl;
-		if(dst > filter_radius){
-			map->EraseNearbyMapPoint(nearby_points[pt_id]);
-			num_filtered++;
-		}
-	}
-	cout << "total points num " << num_pts << " filtered  " << num_filtered << " points " << endl;
-}
-
-void PublishMapPointstoCloud(std::vector<ORB_SLAM2::MapPoint*> points,ros::Publisher publisher,
-								vector<geometry_msgs::Point>* points_pos){
-	int all_map_pts = points.size();
-	// Publish all map points 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr map_cloud(new pcl::PointCloud<pcl::PointXYZ>);	
-	int pts_num = 0;
-	for (int pt_id = 1; pt_id <= all_map_pts; ++pt_id){
-		if (!points[pt_id - 1] || points[pt_id - 1]->isBad()) {
-			// printf("Point %d is bad\n", pt_id);
-			continue;
-		}
-		cv::Mat wp = points[pt_id - 1]->GetWorldPos();
-
-		if (wp.empty()) {
-			printf("World position for point %d is empty\n", pt_id);
-			continue;
-		}
-		if(points_pos){
-			geometry_msgs::Point pt_pos;
-			pt_pos.x = wp.at<float>(0);
-			pt_pos.y = wp.at<float>(1);
-			pt_pos.z = wp.at<float>(2);
-			points_pos->push_back(pt_pos);
-			pts_num++;
-		}		
-		map_cloud->push_back(pcl::PointXYZ(wp.at<float>(0), wp.at<float>(1), wp.at<float>(2)));
-		//printf("Done getting map point %d\n", pt_id);
-	}
-	// if(points_pos){
-	// 	cout << "pts_num in pointscloud is " << pts_num << endl;
-	// }
-	sensor_msgs::PointCloud2 ros_map_cloud;
-	pcl::toROSMsg(*map_cloud, ros_map_cloud);
-	ros_map_cloud.header.frame_id = "map";
-	publisher.publish(ros_map_cloud);
-}
-
-vector<int> getColor(int value){
-	vector<int> startColor{0,255,0};
-	vector<int> endColor{255,0,0};	
-	if(value >= 255)
-		return endColor;
-	else if(value <= 0)
-		return startColor;
-
-	int r_gap=endColor[0]-startColor[0];
-	int g_gap=endColor[1]-startColor[1];
-	int b_gap=endColor[2]-startColor[2];
-	
-	// int nSteps = max(abs(r), max(abs(g), abs(b)));
-	// if (nSteps < 1) nSteps = 1;
-	int nSteps = 255;
-	// Calculate the step size for each color
-	float rStep=r_gap/(float)nSteps;
-	float gStep=g_gap/(float)nSteps;
-	float bStep=b_gap/(float)nSteps;
-
-	// Reset the colors to the starting position
-	float fr=startColor[0];
-	float fg=startColor[1];
-	float fb=startColor[2];	
-
-	// float step = (value - 255)/255;
-	int step = value;
-	return vector<int>{(int)(fr+rStep*step+0.5),(int)(fg+gStep*step+0.5),(int)(fb+bStep*step+0.5)};
-}
-
-void VisualizeCostCube(cv::Mat cost_map,geometry_msgs::Pose camera_pose){
-	if(cost_map.empty()){
-		cout << "CostCube map is empty." << endl;
-		return;
-	}
-	visualization_msgs::MarkerArray markerArr;
-	visualization_msgs::Marker marker;
-	marker.header.frame_id = "map";
-	marker.header.stamp = ros::Time::now();
-	marker.ns = "";
-	marker.lifetime = ros::Duration();	
-	marker.type = visualization_msgs::Marker::CUBE;
-	marker.action = visualization_msgs::Marker::MODIFY;
-	marker.pose.orientation.x = 0.0;
-	marker.pose.orientation.y = 0.0;
-	marker.pose.orientation.z = 0.0;
-	marker.pose.orientation.w = 1.0;
-	marker.scale.x = 0.8 * costcube_resolution;
-	marker.scale.y = 0.8 * costcube_resolution;
-	marker.scale.z = 0.8 * costcube_resolution;
-	marker.color.a = 0.1; // Don't forget to set the alpha!
-
-	visualization_msgs::MarkerArray markerTextArr;
-	visualization_msgs::Marker marker_text;
-	marker_text.header.frame_id = "map";
-	marker_text.header.stamp = ros::Time::now();
-	marker_text.ns = "";
-	marker_text.lifetime = ros::Duration();	
-	marker_text.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
-	marker_text.action = visualization_msgs::Marker::MODIFY;
-	marker_text.pose.orientation.x = 0.0;
-	marker_text.pose.orientation.y = 0.0;
-	marker_text.pose.orientation.z = 0.0;
-	marker_text.pose.orientation.w = 1.0;
-	marker_text.scale.z = 0.3 * costcube_resolution;
-	marker_text.color.a = 1.0; // Don't forget to set the alpha!
-
-	vector<int> cam_posid{int(field_size / costcube_resolution),int(field_size / costcube_resolution),0};
-	int marker_id = 0;
-	for (int row = 0; row < cost_map.size[0]; ++row){
-		for (int col = 0; col < cost_map.size[1]; ++col){
-                        for (int hei = 0;hei < cost_map.size[2]; ++ hei){
-				int cur_cost =  cost_map.at<float>(row, col, hei);
-				// cout << "cur_cost:" << cur_cost << " ";
-				vector<int> color  = getColor(cur_cost);
-				marker.pose.position.x = camera_pose.position.x + (row - cam_posid[0]) * costcube_resolution;
-				marker.pose.position.y = camera_pose.position.y + (col - cam_posid[1]) * costcube_resolution;
-				marker.pose.position.z = camera_pose.position.z + (hei - cam_posid[2]) * costcube_resolution;
-				marker.color.r =  color[0];
-				marker.color.g = color[1];
-				marker.color.b = color[2];
-				marker.id = marker_id++;
-				markerArr.markers.push_back(marker);
-				marker_text.pose = marker.pose;
-				marker_text.text = to_string(cur_cost);
-				marker_text.id = marker_id - 1;
-				markerTextArr.markers.push_back(marker_text);
-			}
-			// cout << endl;
-		}
-		// cout << endl;
-	}
-	// cout << endl << endl;
-	vis_pub.publish(markerArr);
-	vis_text_pub.publish(markerTextArr);
-}
 
 inline bool isInteger(const std::string & s){
 	if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) return false;
